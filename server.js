d513db0babd67c9e7395d529d742a2c91c523715
Jared Sutch <sutch002@gmail.com>
2021-05-19 20:38:36 -0500

    updated script

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { json } = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 //routes
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "db", "db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  const dbJson = require("./db/db.json");
+
+  console.log(dbJson);
+  const dbObj = JSON.parse(dbJson);
+
+  console.log(req.body);
+  const newNote = req.body;
+
+  dbJson.push(newNote);
+  console.log("Updated:");
+  console.log(dbJson);
+
+  fs.writeFile("./db/db.json", JSON.stringify(dbJson), function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+    res.json(newNote);
+  });
 });
 
 app.get("/notes", (req, res) => {
